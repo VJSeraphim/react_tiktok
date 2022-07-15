ae9d976f54c49b30746193cfd31068e388682cfa
VJS <minsung_96@naver.com>
2022-07-15 23:59:18 +0900

    Discover

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,12 +5,15 @@ import Link from 'next/link'
 import GoogleLogin from 'react-google-login'
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai'
 import { ImCancelCircle } from 'react-icons/im'
+import Discover from './Discover'
+import SuggestedAccounts from './SuggestedAccounts'
+import Footer from './Footer'
 
 const Sidebar = () => {
     const [showSidebar, setShowSidebar] = useState(true)
 
-    const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center'
-
+    const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded'
+    const userProfile = false
 
     return (
         <div>
@@ -34,6 +37,40 @@ const Sidebar = () => {
                             </div>
                         </Link>
                     </div>
+                    {!userProfile && (
+                        <div className="px-2 py-4 hidden xl:block">
+                            <p className="text-gray-400">
+                                Sign in to like and comment on Videos.
+                            </p>
+                            <div className="pr-4">
+                                <GoogleLogin 
+                                    clientId=""
+                                    render={(renderProps) => (
+                                        <button
+                                        className="
+                                            bg-white text-lg text-[#F51997] 
+                                            border-[1px] border-[#F51997] 
+                                            font-semibold px-6 py-3 
+                                            rounded-md outline-none w-full mt-3 
+                                            hover:bg-[#F51997]
+                                            hover:text-white cursor-pointer
+                                        "
+                                            onClick={renderProps.onClick}
+                                            disabled={renderProps.disabled}
+                                        >
+                                            Sign In
+                                        </button>
+                                    )} 
+                                    onSuccess={() => {}}
+                                    onFailure={() => {}}
+                                    cookiePolicy='single_host_origin'
+                                />
+                            </div>
+                        </div>
+                    )}
+                    <Discover />
+                    <SuggestedAccounts />
+                    <Footer />
                 </div>
             )}
         </div>
