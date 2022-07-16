6dbcb6f96de7ec70ce34c6f240262fe23c9ea01d
VJS <minsung_96@naver.com>
2022-07-16 23:54:51 +0900

    Footer

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -5,6 +5,12 @@ import { useRouter } from 'next/router'
 import { topics } from '../utils/constants'
 
 const Discover = () => {
+    const router = useRouter()
+    const { topic } = router.query
+
+    const activeTopicStyle = "xl:border-2 hover-:bg-primary xl:border-gray-2-[#F31997] px-3 py-2 rounded xl:rounded-full flex items-center gap2 justify-center cursor-pointer text-[#F51997]"
+    const topicStyle = "xl:border-2 hover-:bg-primary xl:border-gray-200 px-3 py-2 rounded xl:rounded-full flex items-center gap2 justify-center cursor-pointer text-black"
+
     return (
         <div className="xl:border-b-2 xl:border-gray-200 pb-6">
             <p className="text-gray-600 font-semibold m-3 mt-4 hidden xl:block ">
@@ -13,7 +19,7 @@ const Discover = () => {
             <div className="flex gap-3 flex-wrap">
                 {topics.map((item) => (
                     <Link href={`/?topic=${item.name}`} key={item.name}>
-                        <div>
+                        <div className={topic === item.name ? activeTopicStyle : topicStyle}>
                             <span className="font-bold text-2xl xl:text-md">
                                 {item.icon}
                             </span>
