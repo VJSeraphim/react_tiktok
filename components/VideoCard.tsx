8cd9294cfcf2dee86911bb445fec8fce4410ee9b
VJS <minsung_96@naver.com>
2022-07-19 23:58:26 +0900

    Video Card

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -12,11 +12,91 @@ interface IProps {
 }
 
 const VideoCard: NextPage <IProps> = ({ post }) => {
+    const [isHover, setIsHover] = useState(false)
+    const [playing, setPlaying] = useState(false)
+    const [isVideoMuted, setIsVideoMuted] = useState(false)
+    const videoRef = useRef<HTMLVideoElement>(null)
+
+    const onVideoPress = () => {
+        if(playing) {
+            videoRef?.current?.pause()
+            setPlaying(false)
+        } else {
+            videoRef?.current?.play()
+            setPlaying(true)
+        }
+    }
+
     return (
         <div className="flex flex-col border-b-2 border-gray-200 pb-6">
             <div>
-                <div>
-                    
+                <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+                    <div className="md:w-16 md:h-16 w-10 h-10">
+                        <Link href="/">
+                            <>
+                                <Image 
+                                    width={62}
+                                    height={62}
+                                    className="rounded-full"
+                                    src={post.postedBy.image}
+                                    alt="profile photo"
+                                    layout="responsive"
+                                />
+                            </>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link href="/">
+                            <div className="flex items-center gap-2">
+                                <p className="flex gap-2 item-center md:text-md font-bold text-primary">
+                                    {post.postedBy.userName}
+                                    {``}
+                                    <GoVerified className="text-blue-400 text-md" />
+                                </p>
+                                <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                                    {post.postedBy.userName}
+                                </p>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="lg:ml-20 flex gap-4 relative">
+                <div 
+                    className="rounded-3xl"
+                    onMouseEnter={() => setIsHover(true)}
+                    onMouseLeave={() => setIsHover(false)}
+                >
+                    <Link href="/">
+                        <video
+                            ref={videoRef}
+                            src={post.video.asset.url}
+                            loop
+                            className="lg:w-[600px] lg:h=[530px] w-[200px] h-[300px] md:h-[400px] cursor-pointer bg-gray-100 rounded-2xl"
+                        />
+                    </Link>
+                    {isHover && (
+                        <div>
+                            {playing ? (
+                                <button>
+                                    <BsFillPauseFill className="text-black text-2xl lg:text-4xl"/>
+                                </button>
+                            ) : (
+                                <button>
+                                    <BsFillPlayFill className="text-black text-2xl lg:text-4xl"/>
+                                </button>
+                            )}
+                            {isVideoMuted ? (
+                                <button>
+                                    <HiVolumeOff className="text-black text-2xl lg:text-4xl"/>
+                                </button>
+                            ) : (
+                                <button>
+                                    <HiVolumeUp className="text-black text-2xl lg:text-4xl"/>
+                                </button>
+                            )}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
