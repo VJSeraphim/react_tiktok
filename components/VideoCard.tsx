475d2cabd047a56ba8e9c595266de34f75066825
VJS <minsung_96@naver.com>
2022-07-20 23:59:51 +0900

    Google Oauth Update

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -76,22 +76,26 @@ const VideoCard: NextPage <IProps> = ({ post }) => {
                         />
                     </Link>
                     {isHover && (
-                        <div>
+                        <div
+                            className="absolute bottom-6 cursor-pointer left-8
+                            md:left-14 lg:left-0 flex gap-10 lg:justify-between
+                            w-[100px] md:w-[50px] p-3"
+                        >
                             {playing ? (
-                                <button>
+                                <button onClick={onVideoPress}>
                                     <BsFillPauseFill className="text-black text-2xl lg:text-4xl"/>
                                 </button>
                             ) : (
-                                <button>
+                                <button onClick={onVideoPress}>
                                     <BsFillPlayFill className="text-black text-2xl lg:text-4xl"/>
                                 </button>
                             )}
                             {isVideoMuted ? (
-                                <button>
+                                <button onClick={() => setIsVideoMuted(false)}>
                                     <HiVolumeOff className="text-black text-2xl lg:text-4xl"/>
                                 </button>
                             ) : (
-                                <button>
+                                <button onClick={() => setIsVideoMuted(true)}>
                                     <HiVolumeUp className="text-black text-2xl lg:text-4xl"/>
                                 </button>
                             )}
