ca7a97422bdd8a86df3a185c5b6badb0113e9921
VJS <minsung_96@naver.com>
2022-07-21 23:58:07 +0900

    Error fix from configurations & localhost

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,12 +3,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 import Logo from '../utils/tiktik-logo.png'
 
 const Navbar = () => {
+    const user = false
+
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
             <Link href="/">
@@ -21,6 +24,19 @@ const Navbar = () => {
                     />
                 </div>
             </Link>
+            <div>
+                SEARCH
+            </div>
+            <div>
+                {user ? (
+                    <div>Logged in</div>
+                ) : (
+                    <GoogleLogin 
+                        onSuccess={(res) => {}}
+                        onError={() => {}}
+                    />
+                )}
+            </div>
         </div>
     )
 }
