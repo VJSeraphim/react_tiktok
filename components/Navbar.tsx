b7c9150c1f4a464ce3b2cb52c8a33a558bb9b29c
VJS <minsung_96@naver.com>
2022-07-22 23:59:10 +0900

    Add User

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,12 @@ import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 import Logo from '../utils/tiktik-logo.png'
+import { createOrGetUser } from '../utils'
+import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
     const user = false
+    const { userProfile, addUser } = useAuthStore()
 
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -32,8 +35,8 @@ const Navbar = () => {
                     <div>Logged in</div>
                 ) : (
                     <GoogleLogin 
-                        onSuccess={(res) => {}}
-                        onError={() => {}}
+                        onSuccess={(res) => createOrGetUser(res, addUser)}
+                        onError={() => console.log('error')}
                     />
                 )}
             </div>
