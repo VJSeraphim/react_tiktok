464e5d587de9f6a59c9fce0f5b28fe2d1a300e7f
VJS <minsung_96@naver.com>
2022-07-30 23:58:58 +0900

    Post ID def

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -9,8 +9,18 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
 import axios from 'axios'
 
 import { BASE_URL } from '../../utils'
+import { Video } from '../../types'
 
-const Detail = () => {
+interface IProps {
+  postDetails: Video, 
+}
+
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails)
+  const videoRef = useRef(null)
+
+  if(!post) return null
+
   return (
     <div>
       Detail
@@ -18,8 +28,12 @@ const Detail = () => {
   )
 }
 
-export const getServerSideProps = async ({ params: { id } }) => {
+export const getServerSideProps = async ({ params: { id } } : { params: { id: string }})  => {
   const { data } = await axios.get(`${BASE_URL}/api/post/${id}`)
+
+  return {
+    props: { postDetails: data }
+  }
 }
 
 export default Detail
