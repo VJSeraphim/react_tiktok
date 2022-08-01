be3eea9c4fb47cf766fe8177aac1e63976207976
VJS <minsung_96@naver.com>
2022-08-01 23:58:00 +0900

    Video & Videocard Details

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -18,13 +18,27 @@ interface IProps {
 const Detail = ({ postDetails }: IProps) => {
   const [post, setPost] = useState(postDetails)
   const [playing, setPlaying] = useState(false)
-  const videoRef = useRef(null)
+  const [isVideoMuted, setIsVideoMuted] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const router = useRouter()
 
   if(!post) return null
 
   const onVideoClick = () => {
-    
+    if(playing) {
+      videoRef?.current?.pause()
+      setPlaying(false)
+    } else {
+        videoRef?.current?.play()
+        setPlaying(true)
+    }
   }
+
+  useEffect(() => {
+    if(post && videoRef?.current) {
+      videoRef.current.muted = isVideoMuted
+    }
+  }, [post, isVideoMuted])
 
   return (
     <div
@@ -36,7 +50,7 @@ const Detail = ({ postDetails }: IProps) => {
         bg-blurred-img bg-no-repeat bg-cover bg-center items-center"
       >
         <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
-          <p>
+          <p className="cursor-pointer" onClick={() => router.back()}>
             <MdOutlineCancel className="text-white text-[35px]"/>
           </p>
         </div>
@@ -59,6 +73,52 @@ const Detail = ({ postDetails }: IProps) => {
               </button>
             )}
           </div>
+        </div>
+        <div className="absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer">
+        {isVideoMuted ? (
+            <button onClick={() => setIsVideoMuted(false)}>
+                <HiVolumeOff className="text-white text-2xl lg:text-4xl"/>
+            </button>
+        ) : (
+            <button onClick={() => setIsVideoMuted(true)}>
+                <HiVolumeUp className="text-white text-2xl lg:text-4xl"/>
+            </button>
+        )}
+        </div>
+      </div>
+
+      <div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+        <div className="lg:mt-20 mt-10">
+          <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+            <div className="md:w-20 md:h-20 w-16 h-16 ml-4">
+              <Link href="/">
+                <>
+                  <Image 
+                      width={62}
+                      height={62}
+                      className="rounded-full"
+                      src={post.postedBy.image}
+                      alt="profile photo"
+                      layout="responsive"
+                  />
+                </>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <div className="flex items-center gap-2">
+                  <p className="flex gap-2 item-center md:text-md font-bold text-primary">
+                    {post.postedBy.userName}
+                    {``}
+                    <GoVerified className="text-blue-400 text-md" />
+                  </p>
+                  <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                    {post.postedBy.userName}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>          
         </div>
       </div>
     </div>
