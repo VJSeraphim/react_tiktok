63544252f114dd3ce9dbe35a184bb54a742ac876
VJS <minsung_96@naver.com>
2022-08-08 23:57:10 +0900

    Comment Section

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { NextPage } from 'next'
+import { MdOutlineVideocamOff } from 'react-icons/md'
+import { BiCommentX } from 'react-icons/bi'
 
 interface IProps {
     text: string
 }
 
-const NoResults: NextPage <IProps> = ({ text }) => {
+const NoResults: NextPage <IProps> = ({ text }: IProps) => {
     return (
-        <div>
-            NoResults
+        <div className="flex flex-col justify-center items-center h-full w-full">
+            <p className="text-8xl">
+                {text === 'No Comments.' 
+                    ? <BiCommentX /> 
+                    : <MdOutlineVideocamOff />
+                }
+            </p>
+            <p className="text-2xl text-center">
+                {text}
+            </p>
         </div>
     )
 }
