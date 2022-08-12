53b1ddd9050523c0f141d28f3d6151634d59e433
VJS <minsung_96@naver.com>
2022-08-12 23:58:53 +0900

    SuggestedUsers

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,11 +2,20 @@ import create from 'zustand'
 import { persist } from 'zustand/middleware'
 import axios from 'axios'
 
+import { BASE_URL } from '../utils'
+import { AiTwotoneExperiment } from 'react-icons/ai'
+
 const authStore = (set: any) => ({
     userProfile: null,
-
+    allUsers: [],
     addUser: (user: any) => set({ userProfile: user}),
-    removeUser: () => set({ userProfile: null})
+    removeUser: () => set({ userProfile: null}),
+
+    fetchAllUsers: async () => {
+        const response = await axios.get(`${BASE_URL}/api/users`)
+    
+        set({ allUsers: response.data })
+    }
 })
 
 const useAuthStore = create(
